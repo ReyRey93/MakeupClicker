48696a732f1caef91fa47bfa3cb45dd6790444cf
ReyRey93 <reyyancaliskan93@gmail.com>
2026-04-19 23:03:14 -0400

    audio

--- Assets/Scripts Folder/GameManager.cs
+++ Assets/Scripts Folder/GameManager.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
 	[SerializeField] TMP_Text countText;
+	[SerializeField] TMP_Text incomeText;
+	[SerializeField] StoreUpgrade[] storeUpgrades;
+	[SerializeField] int updatesPerSecond = 5;
 
-	int count = 0;
+	[Header("Generator values")]
+	[HideInInspector] public float count = 0;
+	float lastIncomeValue = 0;
+	float nextTimeCheck = 0;
 
-	void ClickAction() {
+	private void Start() {
+		UpdateUI();
+	}
+
+	void Update() {
+		if (nextTimeCheck < Time.timeSinceLevelLoad) {
+		IdleCalculate();
+		nextTimeCheck = Time.timeSinceLevelLoad + (1f / updatesPerSecond);
+		}
+	}
+
+	void IdleCalculate() {
+		float sum = 0;
+		foreach (var storeUpgrade in storeUpgrades)
+		{
+			sum += storeUpgrade.CalculateIncomePerSecond();
+			storeUpgrade.UpdateUI();
+		}
+		lastIncomeValue = sum;
+		count += sum / updatesPerSecond;
+		UpdateUI();
+	}
+
+	public void ClickAction() {
 		count++;
 		UpdateUI();
 	}
 
+	public bool PurchaseAction(int cost) {
+		if (count >= cost) {
+			count -= cost;
+			UpdateUI();
+			return true;
+		}
+		return false;
+	}
+
 	void UpdateUI() {
-		countText.text = count.ToString();
+		countText.text = Mathf.RoundToInt(count).ToString();
+		incomeText.text = lastIncomeValue.ToString() + "/s";
 	}
 }
